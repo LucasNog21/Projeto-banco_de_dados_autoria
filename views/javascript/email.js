29f0eee770a85f46df973af7fedd5e22358a9fcb
Lucas <lucasemanuelndq@gmail.com>
2025-03-03 23:40:20 -0300

    banco lascado

---
+++ views/javascript/email.js
@@ -0,0 +1,22 @@
+const nodemailer = require('nodemailer');
+
+const transport = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+        user: 'projeto.thiago.bdaw@gmail.com',
+        pass: 'bdawprojetothiago'
+    }
+});
+
+transport.sendMail({
+    from: 'Mensagem vinda de: <projeto.thiago.bdaw@gmail.com>',
+    to: 'jose.sarmento@escolar.ifrn.edu.br',
+    subject: 'Mensagem aleatorio',
+    html: '<h1>CELOKO NUM COMPENSA</h1>',
+
+})
+.then((Response) => console.log('Email enviado com sucesso'))
+.catch((error) => console.log('Erro em enviar o email.', error))
+
