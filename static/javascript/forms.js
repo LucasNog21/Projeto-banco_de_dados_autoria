6d0536664f673dd27c3f583abd9fc215e16018af
LucasNog21 <lucasemanuelndq@gmail.com>
2025-03-10 11:45:18 -0300

    gráfico incompleto com o csv

--- static/javascript/forms.js
+++ static/javascript/forms.js
@@ -17,29 +17,10 @@ csvtojson()
             } else if (row[sabe_tecnoautoritarismo] === 'Sim') {
                 sim += 1;
             }
-        });
-        const ctx = document.getElementById('grafico1');
 
-new Chart(ctx, {
-    type: 'pie',
-    data: {
-    labels: ['Sim', 'Não'],
-    datasets: [{
-        label: 'Sabe o que é tecnoautoritarismo',
-        data: [sim, nao],
-        borderWidth: 1
-    }]
-    },
-    options: {
-    scales: {
-        y: {
-        beginAtZero: true
-        }
-    }
-    }
-});
+        
+        })
+        return [sim,nao]
     })
-    .catch((err) => {
-        console.error('Erro ao converter CSV:', err);
-    });
 
+console.log(csvtojson())
